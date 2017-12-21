976213d9ebe3a0349e9ae80e7f510ae62fdbce28
Abayomi Yusuf <yusufabayomi27@yahoo.com>
2017-12-21 11:24:34 +0100

    Almost there

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Posts
 from django.utils import timezone
 
@@ -6,3 +6,8 @@ from django.utils import timezone
 def post_list(request):
     posts = Posts.objects.filter(published_date__lte= timezone.now())
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Posts, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
