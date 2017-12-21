77aa23dff998a9c773c24781cf4e7c1b3b9fea48
Abayomi Yusuf <yusufabayomi27@yahoo.com>
2017-12-21 01:08:09 +0100

    Making sense

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Posts
+from django.utils import timezone
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    posts = Posts.objects.filter(published_date__lte= timezone.now())
+    return render(request, 'blog/post_list.html', {'posts':posts})
